f4278bfee47a82099402f2bb032b66549398a6fb
peter <peter@mail.com>
2012-09-06 15:55:30 -0400

    init

---
+++ src/main/webapp/app/view/Application.js
@@ -0,0 +1,16 @@
+/*
+ * File: public/javascripts/app/view/Application.js
+ */
+
+Ext.define('mye.view.Application', {
+    extend: 'Ext.container.Container',
+    alias: 'widget.app',
+
+
+    initComponent: function() {
+        var me = this;
+
+        me.callParent(arguments);
+    }
+
+});
