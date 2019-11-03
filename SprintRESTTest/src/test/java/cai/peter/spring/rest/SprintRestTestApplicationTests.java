6be68e1a369e72181c4666aaa9f1c745291bd10b
Peter Cai <petercaica@hotmail.com>
2019-11-02 20:15:40 -0400

    init

---
+++ SprintRESTTest/src/test/java/cai/peter/spring/rest/SprintRestTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cai.peter.spring.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintRestTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
