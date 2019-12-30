f7f391315216de733008d575812cfe6d6bbfc629
Peter Cai <petercaica@hotmail.com>
2019-12-30 14:02:23 -0500

    QuickSort

--- algorithm/src/cai/peter/algorithm/array/QuickSort.java
+++ algorithm/src/cai/peter/algorithm/array/QuickSort.java
@@ -34,7 +34,7 @@ public class QuickSort {
   }
 
   @Test
-  public void testK() {
+  public void test() {
     int a[] =
         new int[] {
           49, 38, 65, 97, 76, 13, 27, 49, 78, 34, 12, 64, 5, 4, 62, 99, 98, 54, 56, 17, 18, 23, 34,
