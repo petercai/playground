2786127fe386f9df36defc273ea26d0d088151b8
Peter Cai <petercaica@hotmail.com>
2020-01-01 13:58:48 -0500

    format int

--- algorithm/src/cai/peter/algorithm/array/FormatInt.java
+++ algorithm/src/cai/peter/algorithm/array/FormatInt.java
@@ -1,27 +1,25 @@
 package cai.peter.algorithm.array;
 
-
 import org.junit.Test;
 
 public class FormatInt {
-    public String formatInt(int v) {
-        String s = String.valueOf(v);
-        StringBuilder b = new StringBuilder();
-        int j = s.length();
-        for(int i=0;i<s.length();i++)
-        {
-            if(j--%3==0&&i>0) b.append(',');
-            b.append(s.charAt(i));
-        }
-        return b.toString();
+  public String formatInt(int v) {
+    String s = String.valueOf(v);
+    StringBuilder b = new StringBuilder();
+    int len = s.length();
+    for (int i = 0; i < len; i++) {
+      if (i > 0 && (len - i) % 3 == 0) b.append(',');
+      b.append(s.charAt(i));
     }
+    return b.toString();
+  }
 
-    @Test
-    public void test(){
+  @Test
+  public void test() {
 
-        int v=123456;
-        System.out.println(v);
-        System.out.println(formatInt(v));
-    }
-
+    System.out.println(formatInt(23));
+    System.out.println(formatInt(123));
+    System.out.println(formatInt(1234));
+    System.out.println(formatInt(123456));
+  }
 }
