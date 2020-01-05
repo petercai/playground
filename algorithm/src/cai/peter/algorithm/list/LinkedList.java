37b80128c73482bc71c0f50d69f3ef80518ca9bb
Peter Cai <petercaica@hotmail.com>
2020-01-05 17:47:19 -0500

    cycle list

--- algorithm/src/cai/peter/algorithm/list/LinkedList.java
+++ algorithm/src/cai/peter/algorithm/list/LinkedList.java
@@ -1,49 +1,51 @@
 package cai.peter.algorithm.list;
 
+import org.junit.Test;
+
 import java.util.*;
 
 public class LinkedList {
 
-    public boolean hasCycle(Node head){
-        Node slow = head;
-        Node fast = head;
-        while(fast!=null&&fast.getNext()!=null){
-            slow = slow.getNext();
-            fast = fast.getNext().getNext();
-            if(slow == fast)
-                return true;
-        }
-        return false;
+  public boolean hasCycle(Node head) {
+    if (head == null) return false;
+    Node slow = head, fast = head;
+    while (fast != null && fast.getNext() != null) {
+      slow = slow.getNext();
+      fast = fast.getNext().getNext();
+      if (slow.equals(fast)) return true;
     }
+    return false;
+  }
 
-    public Node reverse(Node head) {
-        if (head == null) return null;
-        Node tmp = head;
-        Node prev = head.getNext();
-        tmp.setNext(null);
+  public Node reverse(Node head) {
+    if (head == null) return null;
+    Node tmp = head;
+    Node prev = head.getNext();
+    tmp.setNext(null);
 
-        while(prev!=null){
-            head = prev;
-            prev = head.getNext();
-            head.setNext(tmp);
-            tmp = head;
-        }
-        return head;
+    while (prev != null) {
+      head = prev;
+      prev = head.getNext();
+      head.setNext(tmp);
+      tmp = head;
     }
+    return head;
+  }
 
+  @Test
+  public void main() {
+    LinkedList e = new LinkedList();
+    List<Integer> l = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+    //    System.out.println(l);
 
-    static public void main(String[] args) {
-        LinkedList e = new LinkedList();
-        List<Integer> l = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
-        System.out.println(l);
+    Node head = ListUtil.valueOf(l);
+    System.out.println("head has cycle=" + hasCycle(head));
 
-        Node head = ListUtil.valueOf(l);
-        ListUtil.createCycle(head,2);
-
-        boolean cycle = e.hasCycle(head);
-        System.out.println("Has cycle="+cycle);
-//        head = e.reverse(head);
-//        List list = ListUtil.toList(head);
-//        System.out.println(list);
-    }
+    ListUtil.createCycle(head, 2);
+    boolean cycle = e.hasCycle(head);
+    System.out.println("cycle has cycle=" + cycle);
+    //        head = e.reverse(head);
+    //        List list = ListUtil.toList(head);
+    //        System.out.println(list);
+  }
 }
--- algorithm/src/cai/peter/algorithm/list/Node.java
+++ algorithm/src/cai/peter/algorithm/list/Node.java
@@ -4,10 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Objects;
+
 @Data
 @RequiredArgsConstructor
 @AllArgsConstructor
 public class Node {
     private final int value;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return value == node.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
     private Node next;
 }
