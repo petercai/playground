d6d42cda4863357d39fbe6fe529f73177c1a0bcc
Peter Cai <petercaica@hotmail.com>
2020-01-04 20:46:22 -0500

    remove duplicate from sorted arrays

--- algorithm/src/cai/peter/algorithm/array/RemoveDuplicateFromArray.java
+++ algorithm/src/cai/peter/algorithm/array/RemoveDuplicateFromArray.java
@@ -1,5 +1,9 @@
 package cai.peter.algorithm.array;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 /*
 Given a sorted array, remove the duplicates in place such that each element appear
 only once and return the new length. Do not allocate extra space for another array,
@@ -8,8 +12,23 @@ For example, given input array A = [ 1 , 1 , 2 ], your function should return le
 and A is now [ 1 , 2 ]
  */
 public class RemoveDuplicateFromArray {
-    public int[] remove(int[] a)
+    public int remove(int[] a)
     {
-        return null;
+        int i = 0;
+        for(int j=1;j<a.length;j++){
+            if(a[i]!=a[j]){
+                i++; // the slow pointer catch up first
+                a[i] = a[j]; // move item back
+            }
+        }
+        return i+1; // the slow one is the pointer of length
+    }
+
+    @Test
+    public void test(){
+        int[] a= new int[]{1,2,2,2,3,5};
+        System.out.print(remove(a));
+        System.out.print(":");
+        System.out.println(Arrays.toString(a));
     }
 }
