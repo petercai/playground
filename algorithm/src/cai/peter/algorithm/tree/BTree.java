dc0789789ec2a330a9ff098d0c19a381304b1676
Peter Cai <petercaica@hotmail.com>
2020-01-10 19:26:55 -0500

    balacned btree

--- algorithm/src/cai/peter/algorithm/tree/BTree.java
+++ algorithm/src/cai/peter/algorithm/tree/BTree.java
@@ -4,34 +4,34 @@ import java.util.Optional;
 
 public class BTree {
 
-  private Node root;
+    private Node root;
 
-  public int height(Node root) {
-    if (root == null) return 0;
-    return Math.max(height(root.getLeft()), height(root.getRight())) + 1;
-  }
+    public int height(Node root) {
+        if (root == null) return 0;
 
-  public boolean isBalance(Node root) {
-    if (root == null) return true;
-
-    int diff = height(root.getLeft()) - height(root.getRight());
-    if (Math.abs(diff) > 1) return false;
-    else return isBalance(root.getLeft()) && isBalance(root.getRight());
-  }
-
-  public void insert(Node root, int v) {
-    if (root == null) root = new Node(v);
-    else {
-      Node left = root.getLeft();
-      Node right = root.getRight();
-      int val = root.getValue();
-      if (v <= val) {
-        if (left == null) root.setLeft(new Node(v));
-        else insert(left, v);
-      } else {
-        if (right == null) root.setRight(new Node(v));
-        else insert(right, v);
-      }
+        return Math.max(height(root.getLeft()), height(root.getRight())) + 1;
     }
-  }
+
+    public boolean isBalance(Node root) {
+        if (root == null) return true;
+        int diff = Math.abs(height(root.getLeft()) - height(root.getRight())); // compare the height(max)
+        if (diff > 1) return false;
+        else return isBalance(root.getLeft()) && isBalance(root.getRight()); // compare all branches
+    }
+
+    public void insert(Node root, int v) {
+        if (root == null) root = new Node(v);
+        else {
+            Node left = root.getLeft();
+            Node right = root.getRight();
+            int val = root.getValue();
+            if (v <= val) {
+                if (left == null) root.setLeft(new Node(v));
+                else insert(left, v);
+            } else {
+                if (right == null) root.setRight(new Node(v));
+                else insert(right, v);
+            }
+        }
+    }
 }
