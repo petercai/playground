8f75a8f513cf15a9835469c04b0cc04539e648c3
Peter Cai <petercaica@hotmail.com>
2020-01-17 16:58:29 -0500

    Bacon

--- algorithm/src/cai/peter/algorithm/graph/kevinbacon/Solution.java
+++ algorithm/src/cai/peter/algorithm/graph/kevinbacon/Solution.java
@@ -1,44 +1,84 @@
 package cai.peter.algorithm.graph.kevinbacon;
+
 import org.junit.Test;
 
 import java.util.*;
+
 public class Solution {
 
-    private static void pm(Object o) { System.out.println(o); }
-    public Map<String,ActorNode> actors;
+  private static void pm(Object o) {
+    System.out.println(o);
+  }
 
-    private class ActorNode {
-        public final String name;
-        public Set<ActorNode> costars;
-        public int baconNumber;
-        public ActorNode(String name) {
-            this.name = name;
-            costars = new HashSet<>();
-            baconNumber = -1;
-        }
-        public void linkActor(ActorNode costar) {
-            if (costar == null) return;
-            costars.add(costar);
-            costar.costars.add(this);
-        }
-        @Override public String toString() {
-            String tmp = name + "(" + baconNumber + ")" + ": ";
-            for (ActorNode an : costars.toArray(new ActorNode[0]))
-                tmp += an.name + ", ";
-            return tmp;
-        }
+  public Map<String, Actor> actors;
+
+  private class Actor {
+    public final String name;
+    public Set<Actor> costars;
+    public int baconNumber;
+
+    public Actor(String name) {
+      this.name = name;
+      costars = new HashSet<>();
+      baconNumber = -1;
     }
+
+    public void linkActor(Actor costar) {
+      if (costar == null) return;
+      costars.add(costar);
+      costar.costars.add(this);
+    }
+
+    @Override
+    public String toString() {
+      String tmp = name + "(" + baconNumber + ")" + ": ";
+      for (Actor an : costars.toArray(new Actor[0])) tmp += an.name + ", ";
+      return tmp;
+    }
+  }
+
+  public void bfsBuildBacon(String actor) {
+    if (!actors.containsKey(actor)) throw new IllegalArgumentException("Invalid actor!");
+    Queue<Actor> q = new LinkedList<>();
+    Actor start = actors.get(actor);
+    start.baconNumber = 0;
+    q.offer(start);
+    while (!q.isEmpty()) {
+      Actor a = q.poll();
+      for (Actor n : a.costars) {
+        if (n.baconNumber == -1) {
+          n.baconNumber = a.baconNumber + 1;
+          q.offer(n);
+        }
+      }
+    }
+  }
+
+  @Test
+  public void testAC_BFS() {
+    this.resetGraph();
+    this.bfsBuildBacon("A");
+    pm(actors.get("C").baconNumber);
+  }
+
+  @Test
+  public void testAD_BFS() {
+    this.resetGraph();
+    this.bfsBuildBacon("A");
+    pm(actors.get("D").baconNumber);
+  }
+
 
     public void resetGraph() {
         actors = new HashMap<>();
-        actors.put("A", new ActorNode("A"));
-        actors.put("B", new ActorNode("B"));
-        actors.put("C", new ActorNode("C"));
-        actors.put("D", new ActorNode("D"));
-        actors.put("E", new ActorNode("E"));
-        actors.put("F", new ActorNode("F"));
-        actors.put("G", new ActorNode("G"));
-        actors.put("H", new ActorNode("H"));
+        actors.put("A", new Actor("A"));
+        actors.put("B", new Actor("B"));
+        actors.put("C", new Actor("C"));
+        actors.put("D", new Actor("D"));
+        actors.put("E", new Actor("E"));
+        actors.put("F", new Actor("F"));
+        actors.put("G", new Actor("G"));
+        actors.put("H", new Actor("H"));
 
         actors.get("A").linkActor(actors.get("B"));
         actors.get("B").linkActor(actors.get("C"));
@@ -47,43 +87,12 @@ public class Solution {
         actors.get("A").linkActor(actors.get("F"));
         actors.get("F").linkActor(actors.get("E"));
 
+        actors.get("A").linkActor(actors.get("C"));
+        actors.get("A").linkActor(actors.get("E"));
+
+
+
         actors.get("G").linkActor(actors.get("H"));
     }
 
-    public Solution() {
-        resetGraph();
-    }
-
-    public void runBacon(String startActor) {
-        if (!actors.containsKey(startActor)) {
-            pm("actor does not exist");
-            return;
-        }
-        Queue<ActorNode> q = new ArrayDeque<>();
-        ActorNode start = actors.get(startActor);
-        start.baconNumber = 0;
-        q.add(start);
-        while (!q.isEmpty()) {
-            ActorNode cur = q.remove();
-            for (ActorNode an : cur.costars.toArray(new ActorNode[0])) {
-                if (an.baconNumber == -1) {
-                    an.baconNumber = cur.baconNumber + 1;
-                    q.add(an);
-                }
-            }
-        }
-    }
-
-    @Test
-    public void testAC(){
-        this.resetGraph();
-        runBacon("A");
-        pm(actors.get("C").baconNumber);
-    }
-
-    public static void main(String[] args) {
-        Solution m = new Solution();
-        m.runBacon("A");
-        pm(m.actors.get("C").baconNumber);
-    }
 }
