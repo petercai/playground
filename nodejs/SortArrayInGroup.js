b62e67b3cb2bab2303d0387e6848170c513a9c55
Peter Cai <petercaica@hotmail.com>
2020-04-16 18:33:20 -0400

    no lodash

--- nodejs/SortArrayInGroup.js
+++ nodejs/SortArrayInGroup.js
@@ -1,19 +1,19 @@
-import _ from "lodash";
 import fs from "fs";
+import _ from "lodash";
 
 let raw = fs.readFileSync("sai.json");
 let json = JSON.parse(raw);
 // console.log(json)
 let sai = json.map((a) =>
-  _.pick(a, [
-    "recordId",
-    "objectName",
-    "fieldName",
-    "fieldLabel",
-    "commonValue",
-    "countryValue",
-    "isExcluded",
-  ])
+    _.pick(a, [
+        "recordId",
+        "objectName",
+        "fieldName",
+        "fieldLabel",
+        "commonValue",
+        "countryValue",
+        "isExcluded",
+    ])
 );
 // console.log(sai)
 
@@ -26,12 +26,23 @@ let sai = json.map((a) =>
 //   return a;
 // }, []);
 //
-// console.log(JSON.stringify(groups,null,2))
 
-let groups = _.groupBy(sai, "fieldLabel");
-console.log(groups);
-// console.log(_.values(groups))
-let res = _.values(groups).flatMap((a) =>
-  a.sort((x, y) => (x.isExcluded === y.isExcluded ? 0 : x.isExcluded ? 1 : -1))
-);
-console.log(res);
+const prop = "fieldLabel"
+const groups = sai.reduce((map, e) => {
+    (map[e[prop]] = map[e[prop]] || []).push(e)
+    return map;
+}, {})
+let vals = Object.values(groups);
+let res = vals.flatMap(a => a.sort((x, y) =>
+    (x.isExcluded === y.isExcluded ? 0 : x.isExcluded ? 1 : -1)
+));
+
+console.log(JSON.stringify(res, null, 2))
+
+// let groups = _.groupBy(sai, "fieldLabel");
+// console.log(groups);
+// // console.log(_.values(groups))
+// let res = _.values(groups).flatMap((a) =>
+//   a.sort((x, y) => (x.isExcluded === y.isExcluded ? 0 : x.isExcluded ? 1 : -1))
+// );
+// console.log(res);
