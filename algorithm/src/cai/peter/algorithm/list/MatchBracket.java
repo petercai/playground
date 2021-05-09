4619fd61cd3768c9eb9774b65fe55d09bfa1831f
Peng Cai <peng.cai@veeva.com>
2021-05-08 23:45:53 -0400

    Update MatchBracket.java

--- algorithm/src/cai/peter/algorithm/list/MatchBracket.java
+++ algorithm/src/cai/peter/algorithm/list/MatchBracket.java
@@ -1,15 +1,19 @@
 package cai.peter.algorithm.list;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.List;
-import java.util.Stack;
+import org.junit.Test;
 
 public class MatchBracket {
   final List<Character> leftBracket = Arrays.asList('(', '[', '{', '<');
   final List<Character> rightBracket = Arrays.asList(')', ']', '}', '>');
 
   public boolean isMatch(String s) {
-    Stack<Character> stack = new Stack<>();
+    Deque<Character> stack = new ArrayDeque<>();
     int pos = -1;
     for (char c : s.toCharArray()) {
       if (leftBracket.contains((c))) {
@@ -19,25 +23,30 @@ public class MatchBracket {
         if (i == pos) stack.pop();
       }
     }
-    return stack.empty();
+    return stack.size()==0;
   }
 
   public boolean isMatch1(String s) {
-    Stack<Character> stack = new Stack<>();
+    Deque<Character> stack = new ArrayDeque<>();
     for (char c : s.toCharArray()) {
       if (c == '(') {
         stack.push(c);
       } else if (c == ')') {
-        if (stack.empty()) return false;
+        if (stack.size()==0) return false;
         else stack.pop();
       }
     }
-    return stack.empty();
+    return stack.size()==0;
   }
 
-  public static void main(String[] args) {
+  @Test
+  public  void test() {
     MatchBracket m = new MatchBracket();
     String s = "(a[}bcd)";
-    System.out.println(s + ": isMatch= " + m.isMatch(s));
+    assertTrue(m.isMatch1(s));
+    assertTrue(m.isMatch1("()"));
+    assertTrue(!m.isMatch1(")("));
   }
+
+
 }
